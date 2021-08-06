0ff34e97494ed4084fe379137ee7aad889ece9bb
Beacher Ward <beacherjr@gmail.com>
2021-08-06 00:59:12 -0400

    second push

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ searchButton.addEventListener("click", ()=>{
     sendApiRequest()
 })
 
-async function getApiRequest(){
+async function sendApiRequest(){
     let API_KEY = "NzHHmzbDy1Hqg4yiVJpZrPCcVd42AvfcEnZEMpOd"
-    let response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${API_KEY}`);
+    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
     console.log(response)
     let data = await response.json()
     console.log(data)
     useApiData(data)
 }
 
-function ApiData(data){
+function useApiData(data){
     document.querySelector("#content").innerHTML += data.explanation
     document.querySelector("#content").innerHTML += `<img src="${data.url}">`
 }
